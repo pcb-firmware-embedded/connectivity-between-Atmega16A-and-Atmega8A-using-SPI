187fc6e1e4b91eeabbf5b3d80d2fba5e00f5fde8
Patrick Almeida <patrickmoros2018@gmail.com>
2026-08-04 16:41:41 -0300

    1 commit

---
+++ Atmega16A/Atmega16A/Atmega16A.c
@@ -0,0 +1,17 @@
+/*
+ * Atmega16A.c
+ *
+ * Created: 8/4/2026 4:40:51 PM
+ *  Author: Administrator
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
