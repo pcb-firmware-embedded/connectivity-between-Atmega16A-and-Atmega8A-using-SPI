500aa0873ca51a3222afe087f540cfad4f874411
Patrick Almeida <patrickmoros2018@gmail.com>
2026-08-04 16:42:41 -0300

    2 commit

---
+++ Atmega8A/Atmega8A/Atmega8A.c
@@ -0,0 +1,17 @@
+/*
+ * Atmega8A.c
+ *
+ * Created: 8/4/2026 4:42:19 PM
+ *  Author: Administrator
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
